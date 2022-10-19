ca9c8a2ffd8cddf8847bc84105ec3078bfcc583d
rounakdhatterwal <rounakdhatterwal43215@gmail.com>
2022-10-19 17:40:45 +0530

    problem1 has been done

---
+++ problem1.js
@@ -0,0 +1,16 @@
+// problem1 : Check whether a number is Prime or not
+
+let num = 15;
+let bag = 0;
+for(let i=0; i<=num; i++){
+if(num%i == 0){
+bag ++;
+}
+}
+if (bag == 2){
+console.log(num, "is a prime number")
+}
+else
+{
+console.log(num, "is not a prime number")
+}
